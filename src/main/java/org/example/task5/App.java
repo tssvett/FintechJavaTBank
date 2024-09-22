a4581b4f265c87196faee199364c4d7e789cf8f7
tssvett <tssvett@mail.ru>
2024-09-22 21:03:59 +0400

    added directories for tasks

---
+++ src/main/java/org/example/task5/App.java
@@ -0,0 +1,11 @@
+package org.example.task5;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class App {
+
+    public static void main(String[] args) {
+        log.info("Hello World!");
+    }
+}
