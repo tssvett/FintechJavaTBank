99bb0a168880dbc13787dd8832189d730641660b
tssvett <tssvett@mail.ru>
2024-10-14 13:52:30 +0400

    added task1, executorService and scheduledExecutorService

--- src/main/java/org/example/task5/App.java
+++ src/main/java/org/example/task5/App.java
@@ -1,4 +1,4 @@
-package org.example.task8;
+package org.example.task5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/org/example/task5/initializer/Initializer.java
+++ src/main/java/org/example/task5/initializer/Initializer.java
@@ -2,5 +2,7 @@ package org.example.task5.initializer;
 
 public interface Initializer {
 
+    void threadingInitializeData();
+
     void initializeData();
 }
